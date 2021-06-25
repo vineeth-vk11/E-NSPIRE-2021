5bb2430b5ffb2c086c0418f8b9223c9a7dca26a5
Pruthvi <pruthvikashyap8765@gmail.com>
2021-06-25 22:13:35 +0530

    Agenda Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,3 +93,82 @@
   });
 
 }(jQuery));
+
+$('.link1').on('click', function() {
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+});
+
+function openCity4(evt, operation) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent4");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks4");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(operation).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen4").click();
+
+
+
+
+function openCity1(evt, operation) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(operation).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen1").click();
+
+
+
+
+function openCity2(evt, operation) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(operation).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen2").click();
+
+
+
+function openCity3(evt, operation) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent3");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks3");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(operation).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen3").click();
+
+
+
+
+
+
